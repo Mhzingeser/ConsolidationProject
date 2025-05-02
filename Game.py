41336b7c86b80c258026f9f93cdfba53f712c888
Tommy Tran <ttran023@terpmail.umd.edu>
2025-05-01 21:55:04 -0400

    Added the first class required for the game, the Card

--- Game.py
+++ Game.py
@@ -4,4 +4,25 @@ import random
 from typing import List, Dict, Optional, Protocol  #These will be needed for their selective-
 #purposes, such as Listing cards on hand, options, protocol (for computer player).
 
-#test commit
+
+# Card Section
+
+class Card:
+    def __init__(self, suit: str, rank: str):    #We will need a constructor for these in order to call on a specific function for the card
+        self.suit = suit
+        self.rank = rank
+    
+    def __str__(self) -> str:
+        return f"{self.rank} of {self.suit}"
+
+    def __repr__(self) -> str:
+        return f"Card('{self.suit}', '{self.rank}')"
+    
+    @property
+    def value(self) -> int:
+        rank_values = {
+            'Ace': 1, '2': 2, '3': 3, '4': 4, '5': 5,
+            '6': 6, '7': 7, '8': 8, '9': 9,
+            '10': 10, 'Jack': 11, 'Queen': 12
+        }
+        return rank_values[self.rank]
